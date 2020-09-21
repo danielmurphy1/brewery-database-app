85523a7142016ea20a35ae11e242f1919fb46f62
Dan <daniel.murphy.1@gmail.com>
2020-09-20 20:53:00 -0400

    inital commit, learning hooks for using state in functional components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import randomcolor from "randomcolor"
 import './App.css';
 
 function App() {
+  const [ value, setValue ] = useState("Testing") 
+    //- array of variable and function - convention is to name function for setting the variable - similar to useState()
+    // - argument of useState is the initial state of the variable
+  const [color, setColor] = useState("")
+  console.log(color)
+
+
+  useEffect(()=>{
+    setColor(randomcolor())
+  }, []) //empty array as second argument only runs when components mounts aka componentDidMount()
+
+  useEffect(()=>{
+    setColor(randomcolor())
+  }, [value]) //second argument array value(s) are the variables it looks for change to run aka componentDidUpdate
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 style={{color: color}}>{value}</h1>
+      <button className="btn btn-danger" onClick={()=>setValue("Test Complete")
+      }>Change</button>
+      <button className = "btn btn-primary" onClick={()=>setValue("Testing")}>Change Back</button>
     </div>
   );
 }
