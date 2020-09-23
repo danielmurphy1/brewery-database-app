fc3e90c318b31092c001862911cf9dafe6fae434
Dan <daniel.murphy.1@gmail.com>
2020-09-22 23:27:45 -0400

    trying to fix input change

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import BreweryCard from "./BreweryCard";
 
 function App() {
   const [breweries, setBreweries] = useState([]);
+  const [breweryName, setBreweryName] = useState("");
+  const [breweryState, setBreweryState] = useState("");
  
 
   useEffect(() => {
@@ -15,7 +17,7 @@ function App() {
       //   console.log(brewery)
       //   setBreweries([...breweries, brewery]);
       // }
-      setBreweries(data)
+      setBreweries(data);
       
       
     }
@@ -24,10 +26,29 @@ function App() {
 
   console.log(breweries)
   
+  function searchBreweryName(e){
+    e.preventDefault();
+    async function fetchData(){
+      const data = await fetch("https://api.openbrewerydb.org/breweries/search?query="+{breweryName}).then(response =>response.json());
+      setBreweries(data);
+    }
+    fetchData();
+    
+    setBreweryName("");
+
+  }
+
+  
+    useEffect(() =>{
+      function handleNameChange(e){
+        setBreweryName(e.target.value);
+      }
+    }, [breweryName])
+  
 
   return (
     <div className="container-fluid">
-      <SearchForm/>
+      <SearchForm breweryName={breweryName} searchBreweryName={searchBreweryName} handleNameChange={useEffect}/>
       <div className="row justify-content-around">
         
           {breweries.map(b => <BreweryCard key={b.id} breweries={b}/>)}
--- src/SearchForm.js
+++ src/SearchForm.js
@@ -1,13 +1,13 @@
 import React from "react"
 
-function SearchForm (){
+function SearchForm (props){
     return(
             <div className="jumbotron">
                 <h1 className="text-center">Brewery Database App</h1>
-                    <form className="mx-auto" style={{maxWidth: 600}}>
+                    <form className="mx-auto" style={{maxWidth: 600}} onSubmit={props.searchBreweryName}>
                         <div className="form-group">
                             <label className="form-label"> Search by Brewery Name: </label>
-                            <input type="text" className="form-control"></input>    
+                            <input type="text" className="form-control" value={props.breweryName} onChange={props.handleNameChange}></input>    
                         </div>
                         <button className="btn btn-primary">Search by Name</button>
                     </form>
